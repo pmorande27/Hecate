66fb95f2f7a55045a3ae266dcb093a4c043e7a86
pmorande27 <pmorande27@gmail.com>
2025-08-06 02:03:28 +0100

    fix bug

--- xml_reconfit.py
+++ xml_reconfit.py
@@ -121,6 +121,7 @@ def calculate_masses_and_error_reconfit(path_to_states,output_path,t0,name_xml="
                     Q = split[2].strip()
                     P = split[3].strip()
                     m = split[4].split(",")[0].strip()
+                    print(m)
                     value = m.split("m= ")[0].strip()
                     error = m.split("+/-")[1].strip()
                     fit_options[name] = {
