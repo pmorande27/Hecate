71413736eb05a38159eef3a000354bfb2c031ff2
pmorande27 <pmorande27@gmail.com>
2025-08-06 02:07:22 +0100

    fix bug

--- xml_reconfit.py
+++ xml_reconfit.py
@@ -121,7 +121,6 @@ def calculate_masses_and_error_reconfit(path_to_states,output_path,t0,name_xml="
                         continue
                     if line.startswith("*"):
                         continue
-                    print(line)
                     split = line.split("|")
                     name = split[0].strip()
                     chisq = split[1].strip()
